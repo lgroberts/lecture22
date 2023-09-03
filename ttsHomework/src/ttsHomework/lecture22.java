2f520b597331f9fe1ac8293e62866b4bd5f00a74
LILIANA ROBERTS <LGROBERT@ASU.EDU>
2023-09-03 13:35:21 -0700

    created demo program to explore errors

---
+++ ttsHomework/src/ttsHomework/lecture22.java
@@ -0,0 +1,22 @@
+package ttsHomework;
+
+public class lecture22 {
+	
+	int 1abc = 5;
+	int d;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     int x;
+	     char c = 10;
+	     
+	     lecture22 newObj = new lecture22();
+	     
+	     System.out.print(newObj.1abc);
+	     System.out.print(newObj.d);
+	     System.out.print(x);
+	     System.out.print(c);
+	}
+
+}
+
